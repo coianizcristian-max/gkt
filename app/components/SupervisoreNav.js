252aec705f220d7d2e12ee6b3f2a364e5701a293
coianizcristian-max <coianiz.cristian@gmail.com>
2026-08-28 09:26:25 +0200

    Supervisore: pagina Metriche (iscritti recenti + record creati 3/7 gg)

--- app/components/SupervisoreNav.js
+++ app/components/SupervisoreNav.js
@@ -13,6 +13,7 @@ const VOCI = [
   { href: '/supervisore/abbonamenti', label: 'Abbonamenti' },
   { href: '/supervisore/coupon', label: 'Coupon' },
   { href: '/supervisore/newsletter', label: 'Newsletter' },
+  { href: '/supervisore/metriche', label: 'Metriche' },
   { href: '/supervisore/versioni', label: 'Versioni' },
 ]
 
